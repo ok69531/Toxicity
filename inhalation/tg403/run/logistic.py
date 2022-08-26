647a0166b111fb1a5a7e7f1b687f22fd8821d2a6
조소영 <ok69531@gmail.com>
2022-08-27 00:11:17 +0900

    tmp

--- inhalation/tg403/run/logistic.py
+++ inhalation/tg403/run/logistic.py
@@ -3,6 +3,9 @@ sys.path.append('../')
 
 import json
 import warnings
+import argparse
+
+from tqdm import tqdm
 
 from utils.read_data import load_data
 from utils.smiles2fing import Smiles2Fing
@@ -23,31 +26,64 @@ from sklearn.metrics import (
 
 warnings.filterwarnings('ignore')
 
-x, y = load_data(inhale_type = 'gas')
 
-params_dict = {
-    'C': [1, 10, 50],
-    'penalty': ['l1', 'l2'],
-    'solver': ['liblinear', 'saga']
-}
-params = ParameterGrid(params_dict)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--inhale-type", type=str, default="vapour")
+    try:
+        args = parser.parse_args()
+    except:
+        args = parser.parse_args([])
 
+    x, y = load_data(inhale_type = args.inhale_type)
 
-x_train, y_train, x_test, y_test = data_split(x, y, seed = 0)
-
-a = CV(x_train, y_train, LogisticRegression, params[0], seed = 0)
-a
-
-
-result = {}
-result['precision'] = []
-result['precision'].append({
-    params[0]: [a['val_precision'], 
-                a['val_recall']]
-})
-
-# pd.DataFrame([a])
-model = LogisticRegression(random_state = 0, **params[0])
-model.fit(x,  y)
+    params_dict = {
+        'C': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 
+              1, 2, 3, 4, 5, 7, 9, 11, 15, 20, 25, 30, 35, 40, 50, 100],
+        'penalty': ['l1', 'l2'],
+        'solver': ['liblinear', 'saga']
+    }
+    params = ParameterGrid(params_dict)
 
 
+    result = {}
+    result['model'] = {}
+    result['precision'] = {}
+    result['recall'] = {}
+    result['f1'] = {}
+    result['accuracy'] = {}
+
+    for p in tqdm(range(len(params))):
+        
+        result['model']['model'+str(p)] = params[p]
+        result['precision']['model'+str(p)] = []
+        result['recall']['model'+str(p)] = []
+        result['f1']['model'+str(p)] = []
+        result['accuracy']['model'+str(p)] = []
+        
+        for seed_ in range(10):
+            x_train, y_train, x_test, y_test = data_split(x, y, seed = seed_)
+            
+            r_ = CV(x_train, 
+                    y_train, 
+                    LogisticRegression, 
+                    params[p], 
+                    seed = seed_)
+            
+            result['precision']['model'+str(p)].append(r_['val_precision'])
+            result['recall']['model'+str(p)].append(r_['val_recall'])
+            result['f1']['model'+str(p)].append(r_['val_f1'])
+            result['accuracy']['model'+str(p)].append(r_['val_accuracy'])
+        
+    json.dump(result, open('../results/' + args.inhale_type + '_logistic.json', 'w'))
+
+
+if __name__ == '__main__':
+    main()
+
+
+# with open('../results/vapour_logistic.json', 'r') as file:
+#     df = json.load(file)
+
+# df['model']
+# [np.mean(df['precision'][i]) for i in df['precision'].keys()]
