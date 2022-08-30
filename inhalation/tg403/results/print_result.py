d1b99a3e2e6f72f535d67e08533acd7c8537f6da
soyeungjo <ok69531@gmail.com>
2022-08-30 22:05:14 +0900

    save model and print metrics

---
+++ inhalation/tg403/results/print_result.py
@@ -0,0 +1,129 @@
+import sys
+sys.path.append('../')
+
+import json
+import pickle
+import sklearn
+import itertools
+
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+from scipy.stats import sem
+from utils.read_data import load_data
+
+
+def mean(values):
+    return np.mean(values).round(3)
+
+
+def se(values):
+    return sem(values).round(3)
+
+
+def metric_mean(data, metric: str):
+    _ = list(map(lambda x: mean(x[1]), data[metric].items()))
+    return _
+
+
+def metric_se(data, metric: str):
+    _ = list(map(lambda x: se(x[1]), data[metric].items()))
+    return _
+
+
+def print_result(inhale_type: str, model: str):
+    with open('cv_results/' + inhale_type + '_' + model + '.json', 'r') as file:
+        d = json.load(file)
+    
+    result_df = pd.DataFrame({'params': d['model'].values(),
+                              'precision_mean': metric_mean(d, 'precision'),
+                              'precision_se': metric_se(d, 'precision'),
+                              'recall_mean': metric_mean(d, 'recall'),
+                              'recall_se': metric_se(d, 'recall'),
+                              'accurcay_mean': metric_mean(d, 'accuracy'),
+                              'accurcay_se': metric_se(d, 'accuracy'),
+                              'f1_mean': metric_mean(d, 'f1'),
+                              'f1_se': metric_se(d, 'f1')
+                })
+    
+    return result_df
+
+
+def print_metrics(df: pd.DataFrame, metric: str):
+    idx = df[metric + '_mean'].idxmax()
+    return df.iloc[idx]
+
+
+def model_save(inhale_type: str, model: str, metric: str):
+    x, y = load_data(inhale_type)
+    result_df = print_result(inhale_type, model)
+    best_params = print_metrics(result_df, metric)['params']
+    
+    if model == 'logistic':
+        from sklearn.linear_model import LogisticRegression
+        clf = LogisticRegression(random_state = 0, **best_params)
+        
+    elif model == 'dt':
+        from sklearn.tree import DecisionTreeClassifier
+        clf = DecisionTreeClassifier(random_state = 0, **best_params)
+    
+    elif model == 'rf':
+        from sklearn.ensemble import RandomForestClassifier
+        clf = RandomForestClassifier(random_state = 0, **best_params)
+    
+    elif model == 'gbt':
+        from sklearn.ensemble import GradientBoostingClassifier
+        clf = GradientBoostingClassifier(random_state = 0, **best_params)
+    
+    elif model == 'xgb':
+        from xgboost import XGBClassifier
+        clf = XGBClassifier(random_state = 0, **best_params)
+    
+    elif model == 'lgb':
+        from lightgbm import LGBMClassifier
+        clf = LGBMClassifier(random_state = 0, **best_params)
+    
+    elif model == 'lda':
+        from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+        clf = LinearDiscriminantAnalysis(**best_params)
+    
+    elif model == 'qda':
+        from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+        clf = QuadraticDiscriminantAnalysis(**best_params)
+    
+    elif model == 'plsda':
+        from sklearn.cross_decomposition import PLSRegression
+        clf = PLSRegression(**best_params)
+        
+    elif model == 'mlp':
+        from sklearn.neural_network import MLPClassifier
+        clf = MLPClassifier(random_state = 0, **best_params)
+    
+    
+    if clf == sklearn.cross_decomposition._pls.PLSRegression:
+        onehot_y = pd.get_dummies(y)
+            
+        clf.fit(x, onehot_y)
+            
+    else:
+        clf.fit(x, y)
+    
+    with open('saved_model/' + inhale_type + '_' + model + '.pkl', 'wb') as file:
+        pickle.dump(clf, file)
+
+
+if __name__ == '__main__':
+    inhale = ['vapour', 'aerosol', 'gas']
+    metrics = ['precision', 'recall', 'accuracy', 'f1']
+    models = ['logistic', 'dt', 'rf', 'gbt', 'xgb', 'lgb', 'lda', 'qda', 'plsda', 'mlp']
+    
+    comb = list(itertools.product(inhale, models))
+    
+    for x in tqdm(comb):
+        model_save(x[0], x[1], 'f1')
+
+
+# if __name__ == '__main__':
+#     df_ = print_result('vapour', 'mlp')
+#     print(print_metrics(df_, 'f1'))
