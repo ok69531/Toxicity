926f70270b1171b546b65c156fa1d89f2e1fd7ce
조소영 <soyoungcho@josoyeong-ui-iMac.local>
2022-09-26 20:22:07 +0900

    category ratio

---
+++ inhalation/tg403/data/ratio.py
@@ -0,0 +1,17 @@
+from locale import normalize
+import openpyxl
+import pandas as pd
+
+vapour = pd.read_excel('vapour.xlsx')
+aerosol = pd.read_excel('aerosol.xlsx')
+gas = pd.read_excel('gas.xlsx')
+
+
+print('count\n', vapour.category.value_counts().sort_index(),
+      '\nratio\n', vapour.category.value_counts(normalize = True).sort_index().round(3))
+
+print('count\n', aerosol.category.value_counts().sort_index(),
+      '\nratio\n', aerosol.category.value_counts(normalize = True).sort_index().round(3))
+
+print('count\n', gas.category.value_counts().sort_index(),
+      '\nratio\n', gas.category.value_counts(normalize = True).sort_index().round(3))
