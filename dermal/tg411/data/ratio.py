b023a412bb7b9fb1c29892e1f6aa6ebd30ac3624
soyeungjo <ok69531@gmail.com>
2022-09-29 13:30:04 +0900

    check the ratio of category

---
+++ dermal/tg411/data/ratio.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('../')
+
+import openpyxl
+import pandas as pd
+from utils.smiles2fing import Smiles2Fing
+
+noael = pd.read_excel('noael.xlsx')
+
+# a, b = Smiles2Fing(gas.SMILES)
+
+print('count\n', noael.category.value_counts().sort_index(),
+      '\nratio\n', noael.category.value_counts(normalize = True).sort_index().round(3))
+
