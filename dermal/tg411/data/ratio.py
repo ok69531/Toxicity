d189cf4f18825bc431ecbb3d334ce0753483f98a
soyeungjo <ok69531@gmail.com>
2022-09-29 13:37:40 +0900

    reconsturct

--- dermal/tg411/data/ratio.py
+++ dermal/tg411/data/ratio.py
@@ -7,8 +7,7 @@ from utils.smiles2fing import Smiles2Fing
 
 noael = pd.read_excel('noael.xlsx')
 
-# a, b = Smiles2Fing(gas.SMILES)
+# a, b = Smiles2Fing(noael.SMILES)
 
 print('count\n', noael.category.value_counts().sort_index(),
       '\nratio\n', noael.category.value_counts(normalize = True).sort_index().round(3))
-
