500f65b1f53cc03a6ba20b716cf4828ea881c66a
soyeungjo <ok69531@gmail.com>
2022-10-02 13:54:01 +0900

    run file

---
+++ genotoxicity/tg473/run/gbt.py
@@ -0,0 +1,91 @@
+import sys
+sys.path.append('../')
+
+import json
+import warnings
+import argparse
+
+from tqdm import tqdm
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    precision_score,
+    recall_score,
+    accuracy_score,
+    f1_score
+)
+
+from utils.read_data import load_data
+from utils.smiles2fing import Smiles2Fing
+from utils.common import ParameterGrid
+
+from rdkit import RDLogger
+from imblearn.over_sampling import SMOTE
+from sklearn.ensemble import GradientBoostingClassifier
+
+warnings.filterwarnings('ignore')
+RDLogger.DisableLog('rdApp.*')
+
+
+def main():
+    x, y = load_data()
+
+    params_dict = {
+        'learning_rate': [0.001, 0.005, 0.01, 0.05, 0.1],
+        'n_estimators': [5, 10, 50, 100, 130],
+        'max_depth': [1, 2, 3, 4],
+        
+    }
+    params = ParameterGrid(params_dict)
+
+
+    result = {}
+    result['model'] = {}
+    result['precision'] = {}
+    result['recall'] = {}
+    result['f1'] = {}
+    result['accuracy'] = {}
+
+    for p in tqdm(range(len(params))):
+        
+        result['model']['model'+str(p)] = params[p]
+        result['precision']['model'+str(p)] = []
+        result['recall']['model'+str(p)] = []
+        result['f1']['model'+str(p)] = []
+        result['accuracy']['model'+str(p)] = []
+        
+        for seed_ in range(10):
+            x_train, x_test, y_train, y_test = train_test_split(x, y, shuffle = True, random_state = seed_)
+            
+            sm = SMOTE(random_state = seed_)
+            x_train, y_train = sm.fit_resample(x_train, y_train)
+            
+            try:
+                model = GradientBoostingClassifier(random_state = seed_, **params[p])
+            except: 
+                model = GradientBoostingClassifier(**params[p])
+                
+            model.fit(x_train, y_train)
+            pred = model.predict(x_test)
+            
+            result['precision']['model'+str(p)].append(precision_score(y_test, pred, average = 'macro'))
+            result['recall']['model'+str(p)].append(recall_score(y_test, pred, average = 'macro'))
+            result['f1']['model'+str(p)].append(f1_score(y_test, pred, average = 'macro'))
+            result['accuracy']['model'+str(p)].append(accuracy_score(y_test, pred))
+            
+            # r_ = CV(x, 
+            #         y, 
+            #         GradientBoostingClassifier, 
+            #         params[p], 
+            #         seed = seed_)
+            
+            # result['precision']['model'+str(p)].append(r_['val_precision'])
+            # result['recall']['model'+str(p)].append(r_['val_recall'])
+            # result['f1']['model'+str(p)].append(r_['val_f1'])
+            # result['accuracy']['model'+str(p)].append(r_['val_accuracy'])
+        
+    json.dump(result, open('../results/test_results/gbt.json', 'w'))
+
+
+if __name__ == '__main__':
+    main()
